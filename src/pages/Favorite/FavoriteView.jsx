bf0940e4edd8f9e83988cce4195d10e634973e43
Fahminur12 <fahmisenp@gmail.com>
2024-11-17 11:57:42 +0700

    FIx Height Not Full

--- src/pages/Favorite/FavoriteView.jsx
+++ src/pages/Favorite/FavoriteView.jsx
@@ -3,7 +3,7 @@ import Card from "../../components/Card"; // Import Card untuk menampilkan film
 
 const FavoriteView = ({ favoriteMovies, onCardClick }) => {
   return (
-    <div className="mx-auto py-10 px-5 -h-screen ">
+    <div className="mx-auto py-10 px-5 min-h-screen ">
       <div className="mt-10 ml-5">
         <h2 className="text-3xl font-bold mb-5">Your Favorite Movies</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 lg:grid-cols-4 gap-1 justify-center mx-auto">
